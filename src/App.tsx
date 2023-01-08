2f0e8f1b7b2345b1b848ea92a4dd388b239088c8
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-01-08 01:09:45 -0300

    inserção da logo no header e começando a desenvolver a side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+
+//styles
+import Global from './styles/Global';
+import dark from './styles/dark';
+import { ThemeProvider } from 'styled-components';
+
+//Components
 import Header from './components/header';
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <ThemeProvider theme={dark}>
+        
+        <Global />
+
+        <Sidebar />
+        <Header />
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Container, Logo } from "./style";
+import { WidthContainer } from "../../styles/Global";
 
-const Header : React.FC = () => {
+const Header: React.FC = () => {
+
+
     return (
-        <h1>header</h1>
+        <WidthContainer>
+            <Container>
+                <div className="container-logo">
+                    <Logo src="https://img.freepik.com/free-icon/test-tube_318-234529.jpg?w=2000" />
+                </div>
+            </Container>
+        </WidthContainer>
     )
 }
 
